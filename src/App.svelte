abbe358efe1d23f9bb4b1a3715d2fc1b810d58e0
zummon <zummon.space@gmail.com>
2023-11-11 12:11:02 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  
+</script>
+
+<div class="h-screen">
+  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-20 border border-gray-200" style="backdrop-filter: blur(20px);">
+      <div class="max-w-md mx-auto text-white">
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, voluptate! Magnam eveniet dolore quaerat ut enim cumque, rem inventore nesciunt ad odio, omnis accusantium dignissimos distinctio quis modi. Officia, odio.
+      </div>
+    </div>
+  </div>
+</div>
